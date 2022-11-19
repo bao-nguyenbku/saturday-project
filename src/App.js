b9405d9061802b358337d145c24e1012c2ff1450
HoangLC19 <hoanglc1911@gmail.com>
2022-11-19 10:12:20 +0700

    "form create"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import logo from './logo.svg';
 import { useContext } from 'react';
 import { AppContext } from './context/app';
+import {BrowserRouter as Router, Routes, Route, Form} from 'react-router-dom';
+import FormCreate from './components/form_create';
+
 function App() {
   const { projectName } = useContext(AppContext);
 
   return (
-    <div className="bg-slate-800 w-screen h-screen">
-      <header className="flex items-center flex-col">
-        <img src={logo} className='w-1/2' alt="logo" />
-        <p className='text-white text-4xl font-bold'>
-          This is {projectName}
-        </p>
-      </header>
+    <div className="app">
+      <FormCreate />
     </div>
   );
 }
