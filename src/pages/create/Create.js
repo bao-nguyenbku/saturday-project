6c1f505247c1d9ab2f0743bf597b4c03d2db0b69
HoangLC19 <hoanglc1911@gmail.com>
2022-11-19 13:13:57 +0700

    "add create form to routes"

---
+++ src/pages/create/Create.js
@@ -0,0 +1,8 @@
+import React from "react";
+import FormCreate from "../../components/formCreate";
+
+const Create = () => {
+    return <FormCreate />;
+}
+
+export default Create;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import HOME_ROUTES from "./pages/home/routes";
 import TEST_ROUTES from "./pages/test/routes";
+import CREATE_ROUTES from "./pages/create/routes";
 
-const Routes = [...HOME_ROUTES, ...TEST_ROUTES];
+const Routes = [...HOME_ROUTES, ...TEST_ROUTES, ...CREATE_ROUTES];
 
 export default Routes;
