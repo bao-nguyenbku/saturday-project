4e803ff1373f035cc741d856632edb1896ff794f
Thinh_NNQT <thinh.ngo0556@hcmut.edu.vn>
2022-11-19 10:17:35 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AppProvider } from './context/app';
+import Update from './updateRoom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      {/* <App /> */}
+      <Update />
     </AppProvider>
   </React.StrictMode>
 );
