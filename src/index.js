adc412d8133990b073e6416b02a35b896bf58a51
Thinh_NNQT <thinh.ngo0556@hcmut.edu.vn>
2022-11-19 13:30:07 +0700

    layout update component

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AppProvider } from './context/app';
-import Update from './components/updateRoom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      {/* <App /> */}
-      <Update />
+      <App />
     </AppProvider>
   </React.StrictMode>
 );
