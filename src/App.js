008e9d3deb33c2fd807e7008369756bdc17b52c3
Huy Hoàng Vũ <hoangvu130301@gmail.com>
2022-11-19 10:55:45 +0700

    add view danh sach

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import logo from './logo.svg';
 import { useContext } from 'react';
 import { AppContext } from './context/app';
+import AccommodationGridView from './components/AccommodationGridView';
+
 function App() {
   const { projectName } = useContext(AppContext);
 
   return (
-    <div className="bg-slate-800 w-screen h-screen">
+    <>
+      {/* <div className="bg-slate-800 w-screen h-screen">
       <header className="flex items-center flex-col">
-        <img src={logo} className='w-1/2' alt="logo" />
-        <p className='text-white text-4xl font-bold'>
-          This is {projectName}
-        </p>
+      <img src={logo} className='w-1/2' alt="logo" />
+      <p className='text-white text-4xl font-bold'>
+      This is {projectName}
+      </p>
       </header>
-    </div>
+      </div> */}
+      <AccommodationGridView/>
+    </>
   );
 }
 
