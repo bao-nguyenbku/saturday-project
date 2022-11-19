0a6fe4ed8dd0353cb974730fbc982a35f218283c
HoangLC19 <hoanglc1911@gmail.com>
2022-11-19 15:10:54 +0700

    "edit form create"

--- src/components/formCreate.js
+++ src/components/formCreate.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { createAccomadation } from "../services/accommodation";
 
 function FormCreate() {
-  const [name, setName] = useState("");
+  const [houseName, setHouseName] = useState("");
+  const [owner, setOwner] = useState("");
   const [phone, setPhone] = useState("");
   const [selectedDistrict, setSelectedDistrict] = useState("quận 1");
+  const [houseNumber, setHouseNumber] = useState("");
   const [street, setStreet] = useState("");
   const [area, setArea] = useState("");
   const [price, setPrice] = useState("");
@@ -13,20 +16,22 @@ function FormCreate() {
 
   const [districts, setDistricts] = useState([]);
 
-
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = JSON.stringify({
-      name: name,
-      phone: phone,
-      district: selectedDistrict,
-      street: street,
-      area: area,
-      price: price,
-      description: description,
-      picture: picture,
-    })
-    await createAccomadation(data)
+    const data = {
+      tên: houseName,
+      soNha: houseNumber,
+      tenDuong: street,
+      quan: selectedDistrict,
+      dienTich: area,
+      gia: price,
+      moTa: description,
+      hinhAnh: picture,
+      chuNha: {
+        ten: owner,
+        soDienThoai: phone,
+      },
+    };
+    await createAccomadation(data);
   };
 
   return (
@@ -37,26 +42,43 @@ function FormCreate() {
             <div className="shadow sm:rounded-md sm:overflow-hidden">
               <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
                 <div className="grid grid-cols-6 gap-6">
-                  <div className="col-span-6 sm:col-span-3">
+                  <div className="col-span-6">
+                    <label
+                      for="house_name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Tên Trọ
+                    </label>
+                    <input
+                      type="text"
+                      name="house_name"
+                      id="house_name"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      onChange={(e) => setHouseName(e.target.value)}
+                      required
+                    />
+                  </div>
+
+                  <div className="col-span-6">
                     <label
                       for="first_name"
                       className="block text-sm font-medium text-gray-700"
                     >
-                      Tên
+                      Tên Chủ Trọ
                     </label>
                     <input
                       type="text"
                       name="first_name"
                       id="first_name"
                       className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      onChange={(e) => setName(e.target.value)}
+                      onChange={(e) => setOwner(e.target.value)}
                       required
                     />
                   </div>
 
-                  <div className="col-span-6 sm:col-span-4">
+                  <div className="col-span-6">
                     <label
-                      for="email_address"
+                      for="phone"
                       className="block text-sm font-medium text-gray-700"
                     >
                       Số điện thoại
@@ -71,7 +93,7 @@ function FormCreate() {
                     />
                   </div>
 
-                  <div className="col-span-6 sm:col-span-3">
+                  <div className="col-span-6">
                     <label
                       for="country"
                       className="block text-sm font-medium text-gray-700"
@@ -96,6 +118,23 @@ function FormCreate() {
 
                   <div className="col-span-6">
                     <label
+                      for="first_name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Số Nhà
+                    </label>
+                    <input
+                      type="text"
+                      name="first_name"
+                      id="first_name"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      onChange={(e) => setHouseNumber(e.target.value)}
+                      required
+                    />
+                  </div>
+
+                  <div className="col-span-6">
+                    <label
                       for="street_address"
                       className="block text-sm font-medium text-gray-700"
                     >
@@ -111,7 +150,7 @@ function FormCreate() {
                     />
                   </div>
 
-                  <div className="col-span-6 sm:col-span-4">
+                  <div className="col-span-6">
                     <label
                       for="email_address"
                       className="block text-sm font-medium text-gray-700"
@@ -172,48 +211,16 @@ function FormCreate() {
 
                 <div>
                   <label className="block text-sm font-medium text-gray-700">
-                    Cover photo
+                    Ảnh Trọ
                   </label>
-                  <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                    <div className="space-y-1 text-center">
-                      <svg
-                        className="mx-auto h-12 w-12 text-gray-400"
-                        stroke="currentColor"
-                        fill="none"
-                        viewBox="0 0 48 48"
-                        aria-hidden="True"
-                      >
-                        <path
-                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        />
-                      </svg>
-                      <div className="flex text-sm text-gray-600">
-                        <label
-                          for="file-upload"
-                          className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                        >
-                          <span>Upload a file</span>
-                          <input
-                            id="file-upload"
-                            name="file-upload"
-                            type="file"
-                            className="sr-only"
-                            onChange={(e) => {
-                              setPicture(e.target.files[0]);
-                              console.log(e.target.files[0]);
-                            }}
-                          />
-                        </label>
-                        <p className="pl-1">or drag and drop</p>
-                      </div>
-                      <p className="text-xs text-gray-500">
-                        PNG, JPG, GIF up to 10MB
-                      </p>
-                    </div>
-                  </div>
+                  <input
+                    type="url"
+                    name="picture"
+                    id="picture"
+                    placeholder="https://example.com"
+                    pattern="https://.*"
+                    size="30"
+                  />
                 </div>
               </div>
               <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
