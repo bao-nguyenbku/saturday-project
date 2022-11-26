aa6dc13abbd258d91e12a22b3ab3056a0177f74f
Thanh Hieu <hieu.dang.dth_is_me@hcmut.edu.vn>
2022-11-26 09:48:57 +0700

    update UI

--- src/pages/create/routes.js
+++ src/pages/create/routes.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 const Create = React.lazy(() => import("./Create"));
 
-const CREATE_ROUTES = [{ path: "/create", element: <Create /> }];
+const CREATE_ROUTES = [{ path: "/create", element: <Suspense loader={<p>Loading ...</p>}><Create /></Suspense> }];
 
 export default CREATE_ROUTES;
