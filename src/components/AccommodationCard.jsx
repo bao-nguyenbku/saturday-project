e81e40f042bc0d016579176e98e09a976357c44d
danhnguyen138 <38952742+danhnguyen138@users.noreply.github.com>
2022-11-26 10:47:39 +0700

    Update AccommodationCard.jsx
    Update Delete

--- src/components/AccommodationCard.jsx
+++ src/components/AccommodationCard.jsx
@@ -1,40 +1,30 @@
 ﻿import * as React from "react";
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import { experimentalStyled as styled } from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
 import { IoLocation, IoTrash, IoPencil } from 'react-icons/io5';
 import styles from './styles.module.css';
 import { formatCurrency } from '../utils/utils';
-import ModalDelete from "./ModalDelete";
-import { useState, useEffect } from "react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export default function AccommodationCard(props) {
   const { data } = props;
 
   const hostInfo = `${props.data.chuNha.ten} - ${props.data.chuNha.soDienThoai}`;
   const [showModal, setShowModal] = React.useState(false);
+  const navigate = useNavigate()
+  
   return (
-    <div className="max-w-sm bg-slate-100 rounded-xl hover:shadow-lg transition-shadow duration-500 ">
+    <div className="max-w-sm bg-slate-100 rounded-xl hover:shadow-lg transition-shadow duration-500 cursor-pointer">
       <div className={styles.inner}>
-        <img src={data.hinhAnh} alt="anh-dai-dien" className="rounded-t-xl"/>
-        <div className="flex absolute top-3 right-3 gap-1">
+        <img src={data.hinhAnh} alt="anh-dai-dien" className="rounded-t-xl h-[200px] w-full object-cover"/>
+        <div className="flex absolute top-3 right-3 gap-1"
+          onClick={()=> {
+            navigate(`/${data.id}/update`)
+          }}
+        >
           <span className="bg-zinc-800/70 p-2 rounded-full">
-            <IoPencil className="text-white" size={25}/>
+              <IoPencil className="text-white" size={25}/>
           </span>
           <span className="bg-zinc-800/70 p-2 rounded-full">
-            <IoTrash className="text-white cursor-pointer" size={25}  onClick={()=>{setShowModal(true)}}/>
-            {/*  */}
+            <IoTrash className="text-white cursor-pointer" size={25} onClick={()=>{setShowModal(true)}} />
             {showModal ? (
         <>
           <div
@@ -68,14 +58,14 @@ export default function AccommodationCard(props) {
                   <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={() => setShowModal(false)}
+                    onClick={() =>setShowModal(false) }
                   >
                     Close
                   </button>
                   <button
                     className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={() => setShowModal(false)}
+                    onClick={() => {navigate(`/${data.id}/delete`)}}
                   >
                     Save Changes
                   </button>
@@ -86,7 +76,6 @@ export default function AccommodationCard(props) {
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
         </>
       ) : null}
-      {/*  */}
           </span>
         </div>
       </div>
@@ -97,8 +86,16 @@ export default function AccommodationCard(props) {
           <p>{[data.soNha, data.tenDuong, data.quan].join(', ')}</p>
         </span>
         <p className="text-sky-700 text-xl font-bold mt-2">{formatCurrency.format(data.gia)} / tháng</p>
+        <div 
+          onClick={()=> {
+            navigate(`/${data.id}/detail`)
+          }} 
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Xem chi tiết
+          <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </div>
       </div>
-     
     </div>
       
   );
